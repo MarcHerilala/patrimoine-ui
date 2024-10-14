f90faa3ce3f8cfb1d0f895b39fac8328bb2a0efb
Marc985 <hei.marc.3@gmail.com>
2024-10-14 21:51:56 +0300

    fix:switch to api local url

--- lib/api-url.ts
+++ lib/api-url.ts
@@ -1,1 +1,2 @@
-export const url="https://hcwq374pyj.execute-api.eu-west-3.amazonaws.com/Prod"
+//export const url="https://hcwq374pyj.execute-api.eu-west-3.amazonaws.com/Prod"
+export const url='http://localhost:8080'
