f633e4012e800ca0a989c4b7e34ff64daf05d0dd
Marc985 <hei.marc.3@gmail.com>
2024-10-14 22:12:47 +0300

    use deployed url

--- lib/api-url.ts
+++ lib/api-url.ts
@@ -1,2 +1,1 @@
-//export const url="https://hcwq374pyj.execute-api.eu-west-3.amazonaws.com/Prod"
-export const url='http://localhost:8080'
+export const url="https://hcwq374pyj.execute-api.eu-west-3.amazonaws.com/Prod"
