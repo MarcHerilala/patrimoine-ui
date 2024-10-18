15e08510a7650fb1d3b223327d2f2b3e91c89bd4
Marc985 <hei.marc.3@gmail.com>
2024-10-18 19:52:51 +0300

    chore:add overflow auto

--- components/dialog.tsx
+++ components/dialog.tsx
@@ -29,7 +29,7 @@ export function DialogBoilerplate({
         
           <Button variant="outline" className="bg-[#161747] text-white py-5 hover:bg-[#161747 hover:text-white] mt-6">{triggerText}</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] flex flex-col gap-2 h-full">
+      <DialogContent className="sm:max-w-[425px] flex flex-col gap-2 h-full overflow-auto">
         <DialogHeader>
           <DialogTitle className="text-center">{title}</DialogTitle>
         </DialogHeader>
